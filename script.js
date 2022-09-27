2734e8368224a91e38c064ae4c0cb5011713f14d
Julian <114091481+JGreen333@users.noreply.github.com>
2022-09-27 12:11:22 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,111 @@
+"use strict";
+
+// DOM ELEMENTS //
+const start = document.getElementById("start");
+const stop = document.getElementById("stop");
+const reset = document.getElementById("reset");
+const secondsElement = document.getElementById("seconds");
+const tensElement = document.getElementById("tens");
+let interval;
+
+// VARIABLES //
+let seconds = 0;
+let tens = 0;
+
+// BUTTON CLICKS //
+start.addEventListener("click", function () {
+  clearInterval(interval);
+  interval = setInterval(startTimer, 10);
+});
+
+stop.addEventListener("click", function () {
+  clearInterval(interval);
+});
+
+reset.addEventListener("click", function () {
+  clearInterval(interval);
+  tens = "00";
+  seconds = "00";
+  tensElement.innerText = tens;
+  secondsElement.innerText = seconds;
+});
+
+// START TIMER //
+
+function startTimer() {
+  tens++;
+
+  if (tens <= 9) {
+    tensElement.innerText = "0" + tens;
+  }
+
+  if (tens > 9) {
+    tensElement.innerText = tens;
+  }
+
+  if (tens > 99) {
+    // console.log("seconds");
+    seconds++;
+    secondsElement.innerText = "0" + seconds;
+    tens = 0;
+    tensElement.innerText = "0" + 0;
+  }
+
+  if (seconds > 9) {
+    secondsElement.innerText = seconds;
+  }
+}
+
+/*
+window.onload = function () {
+  var seconds = 00;
+  var tens = 00;
+  var appendTens = document.getElementById("tens");
+  var appendSeconds = document.getElementById("seconds");
+  var buttonStart = document.getElementById("button-start");
+  var buttonStop = document.getElementById("button-stop");
+  var buttonReset = document.getElementById("button-reset");
+  var Interval;
+
+  buttonStart.onclick = function () {
+    clearInterval(Interval);
+    Interval = setInterval(startTimer, 10);
+  };
+
+  buttonStop.onclick = function () {
+    clearInterval(Interval);
+  };
+
+  buttonReset.onclick = function () {
+    clearInterval(Interval);
+    tens = "00";
+    seconds = "00";
+    appendTens.innerHTML = tens;
+    appendSeconds.innerHTML = seconds;
+  };
+
+  function startTimer() {
+    tens++;
+
+    if (tens <= 9) {
+      appendTens.innerHTML = "0" + tens;
+    }
+
+    if (tens > 9) {
+      appendTens.innerHTML = tens;
+    }
+
+    if (tens > 99) {
+      console.log("seconds");
+      seconds++;
+      appendSeconds.innerHTML = "0" + seconds;
+      tens = 0;
+      appendTens.innerHTML = "0" + 0;
+    }
+
+    if (seconds > 9) {
+      appendSeconds.innerHTML = seconds;
+    }
+  }
+};
+*/
